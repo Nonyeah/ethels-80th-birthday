45f999c4dca0f8f45006b2db016ff4c6f9ab8047
nonyeah <nonyeulasi@hotmail.com>
2025-05-14 01:20:06 +0100

    add trivia component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,14 @@ import Facts from './facts';
 import Address from './location';
 import Registry from './registry';
 import RSVP from './rsvp';
+import TriviaTitle from './Trivia';
 
 
 function App() {
 return (
     <div className="parent-app-container">
     <Intro />
+    <TriviaTitle />
     <Facts />
     <Address />
     <Registry />
---
+++ client/src/Trivia.tsx
@@ -0,0 +1,10 @@
+function TriviaTitle(){
+    return (
+        <div className="trivia-title">
+            <h2>Let's see how much you know about Ethel</h2>
+            <p>Take our 30 second Trivia Pop Quiz!</p>
+        </div>
+    )
+}
+
+export default TriviaTitle
