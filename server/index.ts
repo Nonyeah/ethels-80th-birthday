7b42cef0ffbaebe8e5abf7a0ae121e60e1b6b8e8
nonyeah <nonyeulasi@hotmail.com>
2025-05-16 22:20:56 +0100

    email options added

--- server/index.ts
+++ server/index.ts
@@ -1,17 +1,30 @@
-import express from 'express';
-import cors from 'cors';
-import fs from 'fs';
-import bodyParser from 'body-parser';
+import express from "express";
+import cors from "cors";
+import fs from "fs";
+import path from "path";
+import bodyParser from "body-parser";
+require("dotenv").config();
+const nodemailer = require("nodemailer");
 
 const app = express();
 const PORT = 3000;
 
-app.use(cors({
-  origin: 'https://nonyeah.github.io',
-  methods: ['GET', 'POST'],
-}));
+app.use(
+  cors({
+    origin: "https://nonyeah.github.io",
+    methods: ["GET", "POST"],
+  })
+);
 
 app.use(bodyParser.json());
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.USERNAME,
+    pass: process.env.PASSWORD,
+  },
+});
 
 type Data = {
   name: string;
@@ -20,7 +33,7 @@ type Data = {
   otherguests: string;
 };
 
-app.post('/', (req, res) => {
+app.post("/", (req, res) => {
   console.log("Request body:", req.body);
   const { name, email, attending, otherguests }: Data = req.body;
   const [firstName, lastName] = name.split(" ");
@@ -48,13 +61,46 @@ app.post('/', (req, res) => {
   );
 
   if (attending) {
-    res.send(`That's awesome ${firstName}, we look forward to seeing you on 1st November!`);
+    res.send(
+      `That's awesome ${firstName}, we look forward to seeing you on 1st November!`
+    );
   } else {
     res.send(`Awww that's a pity ${firstName}, we're sorry you can't make it.`);
   }
+
+  interface MailOptions {
+    from: string | undefined;
+    to: string;
+    subject: string;
+    text: string;
+    attachments: Attachments[];
+  }
+
+  type Attachments = { filename: string; path: string };
+
+  const mailOptions: MailOptions = {
+    from: process.env.USERNAME,
+    to: "nonyeulasi@hotmail.com",
+    subject: "Guest List",
+    text: "80th birthday party RSVP list",
+    attachments: [
+      {
+        filename: "invitationList.txt",
+        path: path.join(__dirname, "invitationList.txt"),
+      },
+    ],
+  };
+
+  transporter.sendMail(mailOptions, (error: Error, info: any) => {
+    if (error) {
+      return console.error("Email failed:", error);
+    }
+    console.log("Email sent:", info.response);
+  });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running at https://ethels-80th-birthday.onrender.com:${PORT}`);
+  console.log(
+    `Server running at https://ethels-80th-birthday.onrender.com:${PORT}`
+  );
 });
-
