800f20494ec4e18d1d81409c71e1d73d26c4d4f5
nonyeah <nonyeulasi@hotmail.com>
2025-05-16 23:10:09 +0100

    build app

--- server/dist/index.js
+++ server/dist/index.js
@@ -3,12 +3,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
 const body_parser_1 = __importDefault(require("body-parser"));
-require("dotenv").config();
 const nodemailer = require("nodemailer");
 const app = (0, express_1.default)();
 const PORT = 3000;
