d09bb5c32cea3c6d9c7a86d35c86362b0bc879a8
nonyeah <nonyeulasi@hotmail.com>
2025-05-20 00:37:14 +0100

    modified return message/style

--- client/src/rsvp.tsx
+++ client/src/rsvp.tsx
@@ -10,16 +10,18 @@ interface AttendanceObject {
 function RSVP() {
   const [fullname, setfullname] = useState<string>("Enter your full name");
   const [email, setemail] = useState<string>("Enter your email address");
-  const [otherguests, setotherguests] = useState<string | number >("");
+  const [otherguests, setotherguests] = useState<string | number>("");
   const [accept, setaccept] = useState<boolean>(false);
   const [reject, setreject] = useState<boolean>(false);
-  const messageRef = useRef<HTMLParagraphElement | null>(null);
+  const messageRef = useRef<HTMLDivElement | null>(null);
+  const warning = useRef<HTMLParagraphElement | null>(null);
+  const [isvisible, setisvisible] = useState<boolean>(false);
 
   function send(e: React.FormEvent<HTMLFormElement>) {
     const defaultText = "Enter your full name";
     e.preventDefault();
     if ((!accept && !reject) || !fullname || fullname === defaultText) {
-      messageRef.current!.innerHTML =
+      warning.current!.innerHTML =
         "Please complete all form fields and don't forget \
       to click the accept or reject button";
       return false;
@@ -28,14 +30,14 @@ function RSVP() {
     const pattern = /[<\\>]/;
     const refuse = fullname.match(pattern);
     if (refuse) {
-      messageRef.current!.innerHTML =
+      warning.current!.innerHTML =
         "Only alpha numeric characters allowed. Please \
        enter your name correctly";
       return false;
     }
 
     if (fullname.length > 55) {
-      messageRef.current!.innerHTML =
+      warning.current!.innerHTML =
         "Your name is too long. Please only enter your first \
      and surname";
       return false;
@@ -55,8 +57,11 @@ function RSVP() {
     })
       .then((response: Response) => response.text())
       .then((data: string) => {
-        messageRef.current!.innerHTML = data;
-        messageRef.current!.classList.add("message");
+        setisvisible(true);
+        return data;
+      })
+      .then((data: string) => {
+        messageRef.current!.firstElementChild!.innerHTML = data;
       })
       .then(() =>
         setTimeout(() => {
@@ -65,12 +70,13 @@ function RSVP() {
           setotherguests("");
           setaccept(false);
           setreject(false);
-          messageRef.current!.classList.remove("message");
-          messageRef.current!.innerHTML = "";
+          messageRef.current!.classList.remove("show");
+          messageRef.current!.firstElementChild!.innerHTML = "";
+          setisvisible(false);
         }, 10000)
       )
       .catch((err) => {
-        messageRef.current!.textContent = `Hmmm, seems like there's 
+        warning.current!.textContent = `Hmmm, seems like there's 
         a tech problem: ${err}. Please try again later`;
       });
   }
@@ -94,86 +100,90 @@ function RSVP() {
   }
 
   return (
-    <form onSubmit={send}>
-      <div className="form-container">
-        <p id="browser-message" ref={messageRef}></p>
-        <h2>RSVP</h2>
-
-        <div className="form-name">
-          <label>
-            Your Name:
-            <br />
-            <input
-              id="fname"
-              type="text"
-              name="fname"
-              value={fullname}
-              onChange={(e) => setfullname(e.target.value)}
-              onFocus={handleNameFocus}
-              required
-            />
-          </label>
-        </div>
-
-        <div className="form-email">
-          <label>
-            Email Address:
-            <br />
-            <input
-              id="email"
-              type="email"
-              name="email"
-              value={email}
-              onChange={(e) => setemail(e.target.value)}
-              onFocus={handleEmailFocus}
-            />
-          </label>
-        </div>
-
-        <div className="response-buttons">
-          <button
-            onClick={handleAccept}
-            className={accept ? "accept" : ""}
-            name="accept"
-            type="button"
-          >
-            Joyfully Accepts
-          </button>
-          <button
-            onClick={handleReject}
-            className={reject ? "reject" : ""}
-            type="button"
-            name="reject"
-          >
-            Regretfully Declines
-          </button>
-        </div>
-
-        <div className="form-others">
-          <label>
-            Add Additional Guests:
-            <br />
-            <input
-              id="other"
-              name="other"
-              type="text"
-              inputMode="numeric"
-              pattern="[0-9]{0,1}"
-              value={otherguests}
-              onChange={(e) => {
-                const guestNumber = e.target.value;
-                if (/^\d{0,1}$/.test(guestNumber)) {
-                  setotherguests(e.target.value);
-                }
-              }}
-            />
-          </label>
-        </div>
-        <button id="submit" type="submit">
-          Submit
-        </button>
+    <>
+      <div ref={messageRef} className={isvisible ? "show" : "hide"}>
+        <p></p>
       </div>
-    </form>
+      <form className={isvisible ? "hideForm" : "showForm"} onSubmit={send}>
+        <div className="form-container">
+          <h2>RSVP</h2>
+          <p ref={warning}></p>
+          <div className="form-name">
+            <label>
+              Your Name:
+              <br />
+              <input
+                id="fname"
+                type="text"
+                name="fname"
+                value={fullname}
+                onChange={(e) => setfullname(e.target.value)}
+                onFocus={handleNameFocus}
+                required
+              />
+            </label>
+          </div>
+
+          <div className="form-email">
+            <label>
+              Email Address:
+              <br />
+              <input
+                id="email"
+                type="email"
+                name="email"
+                value={email}
+                onChange={(e) => setemail(e.target.value)}
+                onFocus={handleEmailFocus}
+              />
+            </label>
+          </div>
+
+          <div className="response-buttons">
+            <button
+              onClick={handleAccept}
+              className={accept ? "accept" : ""}
+              name="accept"
+              type="button"
+            >
+              Joyfully Accepts
+            </button>
+            <button
+              onClick={handleReject}
+              className={reject ? "reject" : ""}
+              type="button"
+              name="reject"
+            >
+              Regretfully Declines
+            </button>
+          </div>
+
+          <div className="form-others">
+            <label>
+              Add Additional Guests:
+              <br />
+              <input
+                id="other"
+                name="other"
+                type="text"
+                inputMode="numeric"
+                pattern="[0-9]{0,1}"
+                value={otherguests}
+                onChange={(e) => {
+                  const guestNumber = e.target.value;
+                  if (/^\d{0,1}$/.test(guestNumber)) {
+                    setotherguests(e.target.value);
+                  }
+                }}
+              />
+            </label>
+          </div>
+          <button id="submit" type="submit">
+            Submit
+          </button>
+        </div>
+      </form>
+    </>
   );
 }
 
